a78e7da89bef42322e271431214baae57d5de9dc
filipe <ffilipehhenrique@gmail.com>
2021-07-27 13:53:50 -0300

    Corrigido o bug que impedia o carregamento do script.js

--- Pratica 1/script.js
+++ Pratica 1/script.js
@@ -4,20 +4,20 @@ function carregar() {
     var img = document.querySelector('#img')
     var data = new Date()
     var hora = data.getHours()
-    var min = data.getMinutes()
+
     msg.innerHTML = `Agora são ${hora}Hrs. `
 
     if(hora >= 0 && hora < 12) {
         //bom dia
-        img.src = 'manha certinha.png'
+        img.src = './manha certinha.png'
         document.body.style.background = '#F0D4B6'
 
     } else if(hora < 18) {
         //boa tarde
-        img.src = 'pexels-johannes-plenio-2850287.jpg'
+        img.src = './pexels-johannes-plenio-2850287.jpg'
         document.body.style.background = '#FDB4A5'
     } else if(hora < 24) {
         //boa noite
-        img.src = 'noite certinha.png'
+        img.src = './noite certinha.png'
     }
 }
